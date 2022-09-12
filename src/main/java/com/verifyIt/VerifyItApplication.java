946f5df1496579d9bf9a2336990453dba3ebbb60
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2022-09-12 12:27:14 -0300

    Projeto base

---
+++ src/main/java/com/verifyIt/VerifyItApplication.java
@@ -0,0 +1,13 @@
+package com.verifyIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyItApplication.class, args);
+	}
+
+}
